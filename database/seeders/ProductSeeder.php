deb2b2ce868c93ffa2aca940ad3430123331cb58
Mariam Samy Badr <mariamsamy.1122001@gmail.com>
2023-09-10 21:21:02 +0300

    .

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -13,5 +13,16 @@ class ProductSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table('categories')->insert([ 
+         
+            'id'=>'1',
+            'drug_name'=>'panadol extra',
+            'description'=>'Panadol Extra 500mg/65mg Soluble Effervescent Tablets, paracetamol 500mg, caffeine 65mg. Indications: Relief of mild to moderate pain including rheumatism, neuralgia, musculoskeletal disorders, headache, symptoms of colds and flu, fever, toothache and menstrual pain.',
+            'drug_img'=>''
+
+            
+
+        ]);
+
     }
 }
